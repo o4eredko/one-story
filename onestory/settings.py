d7f20cdc23e719dc87f50d0d487fbc8684a3ed60
o4eredko <evgeny.ocheredko@gmail.com>
2019-11-19 13:05:02 +0200

    Added CORS whitelist

--- onestory/settings.py
+++ onestory/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+	'corsheaders.middleware.CorsMiddleware',
+	'django.middleware.common.CommonMiddleware',
 	'django.middleware.security.SecurityMiddleware',
 	'django.contrib.sessions.middleware.SessionMiddleware',
 	'django.middleware.common.CommonMiddleware',
@@ -125,3 +127,7 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
 	'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTAuthentication',)
 }
+
+CORS_ORIGIN_REGEX_WHITELIST = [
+	r"^http://127.0.0.1:\d+$",
+]
